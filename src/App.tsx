dbb50e8938fe6e208e22bdd99ea90b109a12edb8
beomseok.choi <cbs412@naver.com>
2021-12-17 03:06:12 +0900

    2/2 animation for the reactjs Challenge of nomadcoders

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform, useViewportScroll, AnimatePresence } from "framer-motion";
+import { motion, useMotionValue, useTransform, useViewportScroll, AnimatePresence, Variants } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
+import { Variant } from "@testing-library/react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
+  flex-direction: column;
   align-items: center;
   background:linear-gradient(135deg,rgb(236, 0, 154), rgb(221, 0, 238));
 `;
 
+const Grid = styled.div`
+  display:grid;
+  grid-template-columns:repeat(2, 1fr);
+  gap:10px;
+  margin-bottom:50px;
+`;
+
 
 const Box = styled(motion.div)`
-  width: 200px;
+  width: 400px;
   height: 200px;
   background-color: rgba(255, 255, 255, 0.2);
-  background-color:white;
-  border-radius: 40px;
+  border-radius: 10px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   display:flex;
   justify-content: center;
@@ -27,25 +35,61 @@ const Box = styled(motion.div)`
 `;
 
 const Circle = styled(motion.div)`
-  background-color:#00a5ff;
+  background-color:rgba(255, 255, 255, 1);
   height:100px;
   width:100px;
   border-radius: 50px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-function App() {
-  const [clicked, setClicked] = useState(false);
-  const toggleClicked = () => setClicked(prev => !prev);
+const Overlay = styled(motion.div)`
+  width:100%;
+  height:100%;
+  background-color:rgba(0,0,0,0.5);
+  position:absolute;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+`;
 
+const overlayVariants = {
+  inactive : {
+    opacity:0 //bgcolor 로 바꾸기.
+    
+  },
+  active : {
+    opacity:1
+},
+  exit: {
+    opacity:0
+  }
+}
+
+
+
+function App() {
+  const [boxId, setBoxId] =  useState<null|string>(null);
+  const [switched, setSwitched] = useState(false);
+  const onSwitch = () => {
+    setSwitched(prev => !prev)
+  }
   return (
-    <Wrapper onClick={toggleClicked}>
-      <Box>
-        { !clicked ? <Circle layoutId="circle" style={{borderRadius:50}}></Circle> : null } 
-      </Box>
-      <Box>
-        { clicked ? <Circle layoutId="circle" style={{borderRadius:0}}></Circle> : null } 
-      </Box>
+    <Wrapper >
+      <Grid>
+        {["1", "2", "3", "4"].map(i => (
+        <Box onClick={() => setBoxId(i)} key={i} layoutId={i} whileHover={{scale:1.1}}>
+          {(i === "2" && switched) ? <Circle layoutId="circle"></Circle>  : null }
+          {(i === "3" && !switched) ? <Circle layoutId="circle"></Circle>  : null }
+        </Box>))}
+      </Grid>
+      <button onClick={onSwitch}>Switch</button>
+      <AnimatePresence>
+        { boxId ? <Overlay 
+        onClick={() => setBoxId(null)}
+        variants={overlayVariants} initial="inactive" animate="active" exit="exit">
+          <Box layoutId={boxId} style={{backgroundColor:"rgba(255, 255, 255, 1)", width:390, height:190}}></Box>
+        </Overlay> : null} 
+      </AnimatePresence>
     </Wrapper>
   );
 }
